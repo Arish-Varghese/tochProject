c2c6758fbd980d3cf41b88b178325948f4f3ec80
Arish-Varghese <84323531+Arish-Varghese@users.noreply.github.com>
2021-05-17 20:26:53 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,11 @@
+function purple__button(){
+ document.body.style.background="purple"
+}
+
+function green__button(){
+    document.body.style.background="green"
+}
+
+function yellow__button(){
+    document.body.style.background="yellow"
+}
